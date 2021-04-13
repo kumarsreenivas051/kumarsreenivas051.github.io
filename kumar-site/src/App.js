7959fdb296eda599ab703ee8ea7b50acc2bce6dc
Kumaraswamy V S <kumarvsk630@gmail.com>
2021-04-13 20:32:29 +0530

    React Project Created

---
+++ kumar-site/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <h1>Kumaraswamy V S</h1>
+        <h5>I'm Software Developer with 1.8 years of experience</h5>
+      </header>
+    </div>
+  );
+}
+
+export default App;
